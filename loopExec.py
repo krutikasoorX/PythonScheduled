22ab98edcdb2b6b46d29e3951aa3dfd05e0305fc
krutikasoorX <166822680+krutikasoorX@users.noreply.github.com>
2024-06-27 17:33:41 +0530

    Update loopExec.py

--- loopExec.py
+++ loopExec.py
@@ -1,6 +1,9 @@
 import datetime
 import sys
+import os
 
+n1=os.environ.get('Num1')
+n2=os.environ.get('Num2')
 def loopExecution():
     n=datetime.datetime.now()
     print("We've executed program at ")
@@ -8,4 +11,5 @@ def loopExecution():
 
 loopExecution()
 print(sys.argv[1]+sys.argv[2])
+print(n1+n2)
 
