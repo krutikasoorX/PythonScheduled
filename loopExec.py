afb8b67cb9b141a21fbd498b39141fb1421f23cf
krutikasoorX <166822680+krutikasoorX@users.noreply.github.com>
2024-07-04 18:06:24 +0530

    Update loopExec.py

--- loopExec.py
+++ loopExec.py
@@ -3,14 +3,10 @@ import sys
 import os
 
 print("Edited")
-n1=os.environ.get('Num1')
-n2=os.environ.get('Num2')
 def loopExecution():
     n=datetime.datetime.now()
     print("We've executed program at ")
     print(n)
 
 loopExecution()
-print(n1)
-print(n2)
 
